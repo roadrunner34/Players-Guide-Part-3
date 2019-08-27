ba11b539f89b7e81c926974d75be010aa2fadaa6
roadrunner34 <roadrunner34@users.noreply.github.com>
2019-08-26 19:36:43 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Players_Guide_Part_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
