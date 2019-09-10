b0808e3506e12cde29716ef5779eb584e1877dd3
David Romero <DRomero@dealersocket.com>
2019-09-10 16:01:16 -0600

    Update Book.cs
    initial work on chapter 18

--- Book.cs
+++ Book.cs
@@ -6,5 +6,16 @@ namespace Players_Guide_Part_3
 {
     class Book
     {
+        private string title;
+        private string author;
+        private int pages;
+        private int wordCount;
+
+        public Book(string title, string author)
+        {
+            this.title = title;
+            this.author = author;
+        }
+
     }
 }
