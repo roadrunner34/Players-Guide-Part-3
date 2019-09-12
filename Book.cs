1333bcbe18c40542da86c8f630e80fb6152c62b0
David Romero <DRomero@dealersocket.com>
2019-09-12 09:59:13 -0600

    Update Book.cs

--- Book.cs
+++ Book.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Players_Guide_Part_3
 {
@@ -11,10 +13,27 @@ namespace Players_Guide_Part_3
         private int pages;
         private int wordCount;
 
-        public Book(string title, string author)
+        public Book(string title, string author, int pages, int wordCount)
         {
             this.title = title;
             this.author = author;
+            this.pages = pages;
+            this.wordCount = wordCount;
+        }
+
+        public string GetTitle()
+        {
+            return title;
+        }
+
+        public void SetTitle(string title)
+        {
+            this.title = title;
+        }
+
+        public void AssignWordCountFromText(string text)
+        {
+            wordCount = text.Split(' ').Length;
         }
 
     }
